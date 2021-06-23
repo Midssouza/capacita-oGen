1ea6b6f96acd2bb2b883cda1d9a4459cd3c5a068
Michelle <60756132+Midssouza@users.noreply.github.com>
2021-06-23 18:53:40 -0300

    Add files via upload

---
+++ Tarefa1/EXERCICIO6.java
@@ -0,0 +1,34 @@
+package Tarefa1;
+
+import java.util.Scanner;
+
+public class EXERCICIO6 {
+
+	public static void main(String[] args) {
+		Scanner leia =  new Scanner(System.in);
+		
+		double d, x1, x2, y1, y2;
+		
+		System.out.println("Digite x1: ");
+		x1 = leia.nextDouble();
+		
+		System.out.println("Digite x2: ");
+		x2 = leia.nextDouble();
+		
+		
+		System.out.println("Digite y1: ");
+		y1 = leia.nextDouble();
+		
+		System.out.println("Digite y2: ");
+		y2 = leia.nextDouble();
+		
+		d = Math.sqrt(Math.pow((x2 - x1),2) + Math.pow((y2 - y1), 2));
+		
+		System.out.printf(" Valor de D é %.2f " , d);
+		
+		leia.close();
+		
+
+	}
+
+}
---
+++ Tarefa1/EXERCICIO7.java
@@ -0,0 +1,41 @@
+package Tarefa1;
+
+import java.util.Scanner;
+
+public class EXERCICIO7 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		double a, b, c, d, e, f ;
+		double x , y;
+	
+		System.out.println("Digite numero A: ");
+		a=sc.nextInt();
+		System.out.println("Digite numero B: ");
+		b=sc.nextInt();
+		System.out.println("Digite numero C: ");
+		c=sc.nextInt();
+		System.out.println("Digite numero D: ");
+		d=sc.nextInt();
+		System.out.println("Digite numero E: ");
+		e=sc.nextInt();
+		System.out.println("Digite numero F: ");
+		f=sc.nextInt();
+		
+			x= ((c * e) - (b * f)/ (a * e) - (b * d));
+			System.out.printf("Resultado de x =  %.2f \n ", x);
+		
+			y= ((a * f) - (c * d)/ (a * e) - (b * d));
+			System.out.printf("Resultado de y = %.2f \n ", y);
+		
+			
+			c = (a *x ) + (b *y);
+			System.out.printf("Resultado de c = %.2f \n ", c);
+			
+			f = (d* x) + (e * y);
+			System.out.printf("Resultado de f = %.2f \n ", f);
+			
+		sc.close();
+	}
+	
+}
